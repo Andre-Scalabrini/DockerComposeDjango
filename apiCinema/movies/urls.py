918531b685aeec6265e676aa054e21f1b9d6d5fb
Andre Scalabrini <andrescalabr@gmail.com>
2024-06-18 20:35:02 -0300

    criação projeto prometheus

---
+++ apiCinema/movies/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import MovieListCreate, MovieRetrieveUpdateDestroy
+
+urlpatterns = [
+    path('movies/', MovieListCreate.as_view(), name='movie-list-create'),
+    path('movies/<int:pk>/', MovieRetrieveUpdateDestroy.as_view(), name='movie-detail'),
+]
